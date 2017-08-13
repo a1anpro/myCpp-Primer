37b9f6bc625dbab4bbbc4f366a24726855326c12
yanhui <alanyanhui@gmail.com>
2017-08-13 23:33:26 +0800

    half of chapter 14,it's about overload and conversion

--- 类/Sales_data/Sales_data.cpp
+++ 类/Sales_data/Sales_data.cpp
@@ -1,55 +1,38 @@
-#include <iostream>
-using namespace std;
 #include "Sales_data.h"
 
-//构造函数
-Sales_data::Sales_data(istream &is){
-	read(is, *this);//
-} 
-
-//非成员函数
-ostream& print(ostream &os, const Sales_data &item) {
-	//用传入的流输出 对象
-	os << item.isbn() << " "<< item.units_sold <<" "
-		<<item.revenue << " " << item.avg_price();
-	return os; 
-}
-istream& read(istream &is, Sales_data &item){
-	double price = 0.0;
-	is >> item.bookNo >> item.units_sold >> price;
-	item.revenue = item.units_sold*price;
-	return is;
+Sales_data::Sales_data( istream& is) : Sales_data()
+{
+    is >> *this;
 }
 
- 
-//类成员函数 
-Sales_data& Sales_data::add(const Sales_data &item){
-	if (bookNo != item.bookNo){
-		return *this;
-	}
-	units_sold += item.units_sold;
-	revenue += item.revenue;
-	return *this;
-}
-Sales_data& Sales_data::combine(const Sales_data &rhs){
-	units_sold += rhs.units_sold;
-	revenue += rhs.revenue;
-	return *this;
+Sales_data& Sales_data::operator+=(const Sales_data& rhs)
+{
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
 }
 
-void Sales_data::CalcRevenue(double price){
-	revenue = units_sold * price;//由数量*个数 
-}
-void Sales_data::SetData(Sales_data data){
-	bookNo = data.bookNo;
-	units_sold = data.units_sold;
-	revenue = data.revenue;
+ istream& operator>>( istream& is, Sales_data& item)
+{
+    double price = 0.0;
+    is >> item.bookNo >> item.units_sold >> price;
+    if (is)//check if input is successful 
+        item.revenue = price * item.units_sold;
+    else//input error:item is assigned for default value
+        item = Sales_data();
+    return is;
 }
 
-void  Sales_data::AddData(Sales_data data){
-	if (bookNo != data.bookNo){
-		return ;
-	}
-	units_sold += data.units_sold;
-	revenue += data.revenue;
+ ostream& operator<<( ostream& os, const Sales_data& item)
+{
+    os << item.isbn() << " " << item.units_sold << " " << item.revenue << " "
+       << item.avg_price();
+    return os;
+}
+
+Sales_data operator+(const Sales_data& lhs, const Sales_data& rhs)
+{
+   Sales_data ret = lhs;
+   ret+=rhs;
+   return ret;
 }
--- 类/Sales_data/Sales_data.h
+++ 类/Sales_data/Sales_data.h
@@ -1,55 +1,44 @@
+#ifndef SALES_DATA_H
+#define SALES_DATA_H
+
 #include <iostream>
 #include <string>
 using namespace std;
 
-struct Sales_data{
-	friend ostream& print(ostream &os, const Sales_data &item);
-	friend istream& read(istream &is, Sales_data &item);
+// added overloaded input, output, addition, and compound-assignment operators
+class Sales_data {
+    friend  istream& operator>>( istream&, Sales_data&);       // input
+    friend  ostream& operator<<( ostream&, const Sales_data&); // output
+    friend Sales_data operator+(const Sales_data&,
+                                const Sales_data&); // addition
 
 public:
-//构造函数
-	Sales_data(const string &s, unsigned n, double p):
-				bookNo(s),units_sold(n),revenue(p*n){}//{ cout << "被委托的B构造"<<endl;}
-	
-//委托构造函数
-	//Sales_data():Sales_data(" ",0,0.0){}//{cout << "委托的A构造 -- 默认" <<endl;}
-	//Sales_data(istream &is):Sales_data(){}//{cout << "委托的A构造 -- cin"<< endl; read(is, *this);} 
+    Sales_data(const  string& s, unsigned n, double p)
+        : bookNo(s), units_sold(n), revenue(n * p)
+    {
+    }
+    Sales_data() : Sales_data("", 0, 0.0f) {}
+    Sales_data(const  string& s) : Sales_data(s, 0, 0.0f) {}
+    Sales_data( istream& is);
 
-//#原构造函数 
-	Sales_data() = default;
-	Sales_data(const string &s):bookNo(s){ }
-	Sales_data(istream &is);
+    Sales_data& operator+=(const Sales_data&); // compound-assignment
+     string isbn() const { return bookNo; }
 
-//成员函数 
-	void CalcRevenue(double price);
-	void SetData(Sales_data data);
-	void AddData(Sales_data data);
-	Sales_data &add(const Sales_data &item);
-	Sales_data &combine(const Sales_data &rhs);
-	
-	//返回isbn，加const是为了让常量对象能够调用该函数	
-	string isbn() const{
-		return bookNo;
-	}
-	
 private:
-	inline double avg_price() const;//内联函数的声明和定义写在一起。 
-private:
-	string bookNo;//书的编号
-	unsigned units_sold = 0;//
-	double revenue = 0.0;
+    inline double avg_price() const;
+
+    string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;//revenue means income
 };
 
-inline double Sales_data::avg_price() const{//如果不用const的话，返回的值是不安全的，在print中不能使用 
-	if (units_sold != 0){
-		return revenue / units_sold;
-	}
-	else{
-		return 0.0;
-	}
+istream& operator>>( istream&, Sales_data&);
+ostream& operator<<( ostream&, const Sales_data&);
+Sales_data operator+(const Sales_data&, const Sales_data&);
+
+inline double Sales_data::avg_price() const
+{
+    return units_sold ? revenue / units_sold : 0;
 }
 
-//类定义相关的非成员函数的声明 应该放在和类声明的同一个文件里 
-ostream& print(ostream &os, const Sales_data &item);
-istream& read(istream &is, Sales_data &item);
-
+#endif
